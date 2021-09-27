d35d981bf4c77e011465acb39557b195e7bf0228
Carlos Rojas <caranroja@outlook.com>
2021-09-27 11:04:53 -0500

    Se organiza el formulario de usuarios para que quede en dos columnas

--- src/pages/actualizarRoles.jsx
+++ src/pages/actualizarRoles.jsx
@@ -2,22 +2,25 @@ function ActualizarRoles() {
     return <div>
         <h1 className="breadCrumb">Modulo de usuarios > Actualizar información</h1>
         <form action="managerRoles" className = 'formulario'>
-            <label htmlFor="">Id usuario: </label>
+        <div className="formulariodiv">
+        <label htmlFor="">Id usuario: </label>
             <input type="text" placeholder='Id usuario'/>
             <label htmlFor="">Nombre: </label>
             <input type="text" placeholder='Nombre'/>
             <label htmlFor="">Apellido: </label>
             <input type="text" placeholder='Apellido'/>
             <label htmlFor="">No. documento: </label>
-            <input type="text" placeholder='No. documento'/>
-            <label htmlFor="">Correo electronico </label>
-            <input type="email" placeholder='Correo electronico'/>
-            <label htmlFor="">Estado: </label>
+            <input type="text" placeholder='No. documento'/>            
+        </div>
+        <div className="formulariodiv">
+        <label htmlFor="">Correo electronico </label>
+        <input type="email" placeholder='Correo electronico'/>
+        <label htmlFor="">Estado: </label>
             <select name="estadoRol" id="estadoRol">
-            <option value="selectEstado">Elija un estado:</option>
-            <option value="Autorizado">Autorizado</option>
-            <option value="NoAutorizado">No autorizado</option>
-            <option value="Pendiente">Pendiente</option>
+                <option value="selectEstado">Elija un estado:</option>
+                <option value="Autorizado">Autorizado</option>
+                <option value="NoAutorizado">No autorizado</option>
+                <option value="Pendiente">Pendiente</option>
             </select>
             <label htmlFor="">Rol: </label>
             <select name="estadoRol" id="estadoRol">
@@ -26,7 +29,8 @@ function ActualizarRoles() {
             <option value="Vendedor">Vendedor</option>
             </select>
             <label htmlFor="">Fecha ingreso: </label>
-            <input type="text" placeholder='AAAA/MM/DD HH:MM:SS'/>        
+            <input type="text" placeholder='AAAA/MM/DD HH:MM:SS'/>
+        </div>                   
         </form>
         <div className='ContenedorBotonesGuardar'>
             <button className= 'botonEdit colorBotonEdit'>Guardar</button>
