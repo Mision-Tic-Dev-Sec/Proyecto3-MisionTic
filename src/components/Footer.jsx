4a63ca3f7a8a4ac4e497f14a98060474661d8d28
Carlos Rojas <caranroja@outlook.com>
2021-10-10 13:10:36 -0500

    Se agregan etiquetas < a>  en el footer, para linkar con las redes sociales de los respectivos iconos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,11 @@ const Footer = () => {
       <li>Telefono: 325478921</li>
       <li>Direccion: cra #12-03</li>
       <li>Colombia</li>
-      <li><i class="fab fa-facebook px-2"></i><i class="fab fa-twitter px-2"></i><i class="fab fa-instagram px-2"></i></li>
+      <li>
+        <a href="https://es-la.facebook.com/"><i class="fab fa-facebook px-2"></i></a>
+        <a href="https://twitter.com/?lang=es"><i class="fab fa-twitter px-2"></i></a>
+        <a href="https://www.instagram.com/?hl=es"><i class="fab fa-instagram px-2"></i></a>
+      </li>
     </ul>
   </div>;
 };
